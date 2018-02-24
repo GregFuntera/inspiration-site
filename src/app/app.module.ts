8a05cccb39d8644f878c5f8fd5c4d9b8ffeb3ae1
Greg Funtera <greg.funtera@gmail.com>
2018-02-24 23:13:54 +0800

    - Added carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routingComponents} from './app-routing/app-routing.module';
 import { MaterialModuleModule } from './material-module/material-module.module';
+import { CarouselComponent } from './home/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
+    CarouselComponent,
   ],
   imports: [
     FormsModule,
